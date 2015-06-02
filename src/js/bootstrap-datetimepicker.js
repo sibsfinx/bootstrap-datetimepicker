7e7da50f65fd287d94b173645703e374a7e25131
Alexander Mescheryakov <sibsfinx@ya.ru>
2015-06-02 10:40:46 +0300

    fix widget position and left/right logic
    Conflicts:
    	build/js/bootstrap-datetimepicker.min.js
    	src/js/bootstrap-datetimepicker.js

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -376,6 +376,11 @@
                     element.children().first().after(widget);
                 }
 
+                position = {
+                  left: (component || element).offset().left - $(parent).offset().left,
+                  top: (component || element).offset().top - $(parent).offset().top
+                };
+
                 // Top and bottom logic
                 if (vertical === 'auto') {
                     if (offset.top + widget.height() * 1.5 >= $(window).height() + $(window).scrollTop() &&
@@ -388,8 +393,10 @@
 
                 // Left and right logic
                 if (horizontal === 'auto') {
-                    if (parent.width() < offset.left + widget.outerWidth() / 2 &&
-                        offset.left + widget.outerWidth() > $(window).width()) {
+                    if ((parent.width() < offset.left + widget.outerWidth() / 2 &&
+                        offset.left + widget.outerWidth() < $(window).width()) ||
+                        offset.left - widget.outerWidth() < 0
+                       ) {
                         horizontal = 'right';
                     } else {
                         horizontal = 'left';
@@ -419,15 +426,11 @@
                     throw new Error('datetimepicker component should be placed within a relative positioned container');
                 }
 
-                position = {
-                  left: (component || element).offset().left - $(parent).offset().left,
-                  top: (component || element).offset().top - $(parent).offset().top
-                };
-
                 widget.css({
-                  top: vertical === 'top' ? position.top - (component || element).outerHeight()/2 - widget.outerHeight() : position.top + (component || element).outerHeight(),
-                  left: horizontal === 'left' ? position.left + (component || element).outerWidth() - widget.outerWidth() : position.left + (component || element).outerWidth()/2
+                  top: vertical === 'top' ? position.top - (component || element).outerHeight() / 2 - widget.outerHeight() : position.top + (component || element).outerHeight(),
+                  left: horizontal === 'left' ? position.left + (component || element).outerWidth() - widget.outerWidth() : position.left + (component || element).outerWidth() / 2
                 });
+
             },
 
             notifyEvent = function (e) {
